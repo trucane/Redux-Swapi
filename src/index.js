4205e2e869246f4f65e5b39cec50d86366a8fc5c
trucane <trufashionista2013@gmail.com>
2019-06-19 17:00:33 -0500

    I can display Star Wars  character names

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+
+
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware} from "redux";
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import rootReducer from "./reducers";
 // needed dependancies
 // applyMiddleware from redux
@@ -11,9 +15,10 @@ import rootReducer from "./reducers";
 // logger from redux-logger
 // rootReducer from ./reducers
 
+const middleware = applyMiddleware(logger, thunk)
 const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
+  rootReducer,
+  middleware
 );
 
 ReactDOM.render(
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,10 +1,33 @@
-import /* we need our action types here*/ "../actions";
+import{FETCH_START, FETCH_SUCCESS,FETCH_ERROR}from  "../actions";
 const initialState = {
-  characters: []
+  characters: [],
+  error:'',
+  isLoading:false
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_START:
+      return{
+        ...state,
+        error:'',
+        isLoading:true
+      }
+
+    case FETCH_SUCCESS:
+      return{
+        ...state,
+        isLoading:false,
+        characters:action.payload
+      }
+
+
+    case FETCH_ERROR:
+      return{
+        ...state,
+        isLoading:false,
+        error: "Dude you screwed up some where"
+      }
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
